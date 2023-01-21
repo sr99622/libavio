4fe66c3c1f40c97ec37a8345b4e3f9e648fd28e8
Stephen Rhodes <sr99622@gmail.com>
2023-01-21 15:00:46 -0500

    build out progress widget

--- samples/gui/include/glwidget.h
+++ samples/gui/include/glwidget.h
@@ -20,14 +20,23 @@ public:
 
     static void start(void* widget);
     static void renderCallback(void* caller, const avio::Frame& f);
+    static void progressCallback(void* caller, float pct);
 
     QImage img;
     avio::Frame f;
     QMutex mutex;
     avio::Process* process = nullptr;
 
+signals:
+    void mediaPlayingStarted(qint64);
+    void mediaPlayingStopped();
+    void mediaProgress(float);
+
 protected:
     void paintEvent(QPaintEvent* event) override;
+
+public slots:
+    void seek(float);
 
 };
 
--- samples/gui/src/mainwindow.cpp
+++ samples/gui/src/mainwindow.cpp
@@ -13,13 +13,20 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
     btnStop = new QPushButton("Stop");
     connect(btnStop, SIGNAL(clicked()), this, SLOT(onBtnStopClicked()));
 
+    progress = new Progress(this);
+
     glWidget = new GLWidget();
+    connect(glWidget, SIGNAL(mediaPlayingStarted(qint64)), this, SLOT(mediaPlayingStarted(qint64)));
+    connect(glWidget, SIGNAL(mediaPlayingStopped()), this, SLOT(mediaPlayingStopped()));
+    connect(glWidget, SIGNAL(mediaProgress(float)), this, SLOT(mediaProgress(float)));
+    connect(progress, SIGNAL(seek(float)), glWidget, SLOT(seek(float)));
 
     QWidget* pnlMain = new QWidget();
     QGridLayout* lytMain = new QGridLayout(pnlMain);
-    lytMain->addWidget(btnPlay,   0, 1, 1, 1, Qt::AlignCenter);
-    lytMain->addWidget(btnStop,   1, 1, 1, 1, Qt::AlignCenter);
-    lytMain->addWidget(glWidget,  0, 0, 4, 1);
+    lytMain->addWidget(btnPlay,     0, 1, 1, 1, Qt::AlignCenter);
+    lytMain->addWidget(btnStop,     1, 1, 1, 1, Qt::AlignCenter);
+    lytMain->addWidget(glWidget,    0, 0, 4, 1);
+    lytMain->addWidget(progress,    4, 0, 1, 1);
     lytMain->setColumnStretch(0, 10);
     setCentralWidget(pnlMain);
 }
@@ -29,16 +36,38 @@ MainWindow::~MainWindow()
 
 }
 
+void MainWindow::setPlayButton()
+{
+    if (glWidget->process) {
+        if (glWidget->process->isPaused()) {
+            btnPlay->setText("Play");
+        }
+        else {
+            btnPlay->setText("Pause");
+        }
+    }
+    else {
+        btnPlay->setText("Play");
+    }
+}
+
 void MainWindow::onBtnPlayClicked()
 {
     std::cout << "play" << std::endl;
-    glWidget->play();
+    if (glWidget->process) {
+        glWidget->process->togglePaused();
+    }
+    else {
+        glWidget->play();
+    }
+    setPlayButton();
 }
 
 void MainWindow::onBtnStopClicked()
 {
     std::cout << "stop" << std::endl;
     glWidget->stop();
+    setPlayButton();
 }
 
 void MainWindow::closeEvent(QCloseEvent* event)
@@ -47,4 +76,24 @@ void MainWindow::closeEvent(QCloseEvent* event)
     while (glWidget->process) {
         QThread::msleep(1);
     }
+}
+
+void MainWindow::mediaPlayingStarted(qint64 duration)
+{
+    std::cout << "MainWindow::mediaPlayingStarted" << std::endl;
+    progress->setDuration(duration);
+    setPlayButton();
+}
+
+void MainWindow::mediaPlayingStopped()
+{
+    std::cout << "MainWindow::mediaPlayingStopped" << std::endl;
+    progress->setProgress(0);
+    setPlayButton();
+}
+
+void MainWindow::mediaProgress(float arg)
+{
+    //sldProgress->setValue(std::round(sldProgress->maximum() * arg));
+    progress->setProgress(arg);
 }
