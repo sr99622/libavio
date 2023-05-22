5f85587bee6da1d511e61580e99c810458515385
Stephen Rhodes <sr99622@gmail.com>
2023-05-22 18:06:59 -0400

    windows pip install

--- cmake/FindFFmpeg.cmake
+++ cmake/FindFFmpeg.cmake
@@ -1,27 +1,26 @@
-#*******************************************************************************
-# FindFFmpeg.cmake
+#/********************************************************************
+# libavio/cmake/FindFFmpeg.cmake
 #
-# Copyright (c) 2022 Stephen Rhodes 
+# Copyright (c) 2022  Stephen Rhodes
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
-#******************************************************************************/
+#*********************************************************************/
 
 macro(find_component_library var library_name)
     find_library(${var} NAME ${library_name}
         HINTS
+            $ENV{FFMPEG_INSTALL_DIR}/lib
             $ENV{CONDA_PREFIX}
     )
 endmacro()
@@ -29,6 +28,7 @@ endmacro()
 macro(find_component_include_dir var header_name)
     find_path(${var} ${header_name}
         HINTS
+            $ENV{FFMPEG_INSTALL_DIR}/include
             $ENV{CONDA_PREFIX}
     )
 endmacro()
