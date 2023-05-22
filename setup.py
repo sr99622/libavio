a5a3e0ebe5fe7eb02797b3317d8450c3293b7b94
Stephen Rhodes <sr99622@gmail.com>
2023-05-22 18:06:59 -0400

    windows pip install

--- setup.py
+++ setup.py
@@ -1,96 +1,67 @@
 import os
-import re
-import subprocess
 import sys
-
+import subprocess
+import distutils.ccompiler
+from pathlib import Path
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-PLAT_TO_CMAKE = {
-    "win32": "Win32",
-    "win-amd64": "x64",
-    "win-arm32": "ARM",
-    "win-arm64": "ARM64",
-}
+PKG_NAME = "avio"
+VERSION = "2.1.7"
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
-
 class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
-
-        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
-        cfg = "Debug" if debug else "Release"
-
-        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+        extdir = os.path.join(extdir, PKG_NAME)
 
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
-            f"-DCMAKE_BUILD_TYPE={cfg}",
             f"-DWITHOUT_LIBS=1",
-            f"-Wno-dev",
-        ]
-        build_args = []
-
-        if "CMAKE_ARGS" in os.environ:
-            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
-
-        if self.compiler.compiler_type != "msvc":
-            if not cmake_generator:
-                try:
-                    import ninja  # noqa: F401
-
-                    cmake_args += ["-GNinja"]
-                except ImportError:
-                    pass
-
-        else:
-
-            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
-            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
-
-            if not single_config and not contains_arch:
-                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
-
-            if not single_config:
-                cmake_args += [
-                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
-                ]
-                build_args += ["--config", cfg]
-
-        if sys.platform.startswith("darwin"):
-            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
-            if archs:
-                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
-
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            if hasattr(self, "parallel") and self.parallel:
-                build_args += [f"-j{self.parallel}"]
+            f"-Wno-dev",]
+        
+        if sys.platform == "win32":
+            cmake_args.append(f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE={extdir}")
+        
+        # set environment variables for local cmake scripts, allow user override
+        if 'FFMPEG_INSTALL_DIR' not in os.environ:
+            ffmpeg_dir = os.path.join(Path(__file__).parent.absolute(), "ffmpeg")
+            os.environ['FFMPEG_INSTALL_DIR'] = ffmpeg_dir
+        if 'SDL2_INSTALL_DIR' not in os.environ:
+            sdl_dir = os.path.join(Path(__file__).parent.absolute(), "sdl")
+            os.environ['SDL2_INSTALL_DIR'] = sdl_dir
 
         build_temp = os.path.join(self.build_temp, ext.name)
         if not os.path.exists(build_temp):
             os.makedirs(build_temp)
 
         subprocess.run(["cmake", ext.sourcedir] + cmake_args, cwd=build_temp)
-        subprocess.run(["cmake", "--build", "."] + build_args, cwd=build_temp)
+        subprocess.run(["cmake", "--build", ".", "--config", "Release"], cwd=build_temp)
+
+def get_package_data():
+    data = []
+    shared_lib_extension = distutils.ccompiler.new_compiler().shared_lib_extension
+    for f in os.listdir(PKG_NAME):
+        _, extension = os.path.splitext(f)
+        if extension == shared_lib_extension:
+            data.append(f)
+    return data
 
 setup(
-    name="avio",
-    version="2.1.7",
+    name=PKG_NAME,
+    version=VERSION,
     author="Stephen Rhodes",
     author_email="sr99622@gmail.com",
     description="A python media processing module",
     long_description="",
-    ext_modules=[CMakeExtension("avio")],
+    ext_modules=[CMakeExtension(PKG_NAME)],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.8",
+    packages=[PKG_NAME],
+    package_data = { PKG_NAME : get_package_data() }
 )
