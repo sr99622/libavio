31f5e94f9bac60b0335e14476001f2554cfbf422
Stephen Rhodes <sr99622@gmail.com>
2026-04-14 14:42:09 -0400

    windows reconciliation

--- scripts/windows/windows_build.bat
+++ scripts/windows/windows_build.bat
@@ -1,18 +1,5 @@
 @echo off
 
-if not exist "%ALLUSERSPROFILE%\chocolatey\bin\" (
-    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-)
-if not exist "%ProgramFiles%\Git\" (
-    choco install -y git
-)
-if not exist "%ProgramFiles%\CMake\" (
-    choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System' --apply-install-arguments-to-dependencies
-)
-if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\" (
-    winget install Microsoft.VisualStudio.2022.BuildTools --silent --override "--wait --quiet --add ProductLang En-us --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended"
-)
-
 cd %HOMEPATH%
 if not exist onvif-gui-win-libs\ (
     git clone https://github.com/sr99622/onvif-gui-win-libs
@@ -29,9 +16,10 @@ if exist wheelhouse\ (
 
 call %HOMEPATH%\libavio\scripts\windows\python\install
 call %HOMEPATH%\libavio\scripts\windows\env_variables
-call %HOMEPATH%\libavio\scripts\windows\copy_libs
+rem call %HOMEPATH%\libavio\scripts\windows\copy_libs
 
-set list=(310 311 312 313 314)
+rem set list=(310 311 312 313 314)
+set list=(313)
 for %%v in %list% do (
     cd %HOMEPATH%
     %LOCALAPPDATA%\Programs\Python\Python%%v\python -m venv py%%v
@@ -41,6 +29,6 @@ for %%v in %list% do (
     pip install build delvewheel
     cd libavio
     python -m build --sdist --wheel
-    delvewheel repair dist\*cp%%v-cp%%v-*.whl --add-path avio
+    delvewheel repair dist\*cp%%v-cp%%v-*.whl --add-path %HOMEPATH%\onvif-gui-win-libs\ffmpeg\bin --add-path %HOMEPATH%\onvif-gui-win-libs\sdl\bin
     call deactivate
 )
