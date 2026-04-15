157f8d5f81b5c5669c63d9e3f9331d4b78329f7e
Stephen Rhodes <sr99622@gmail.com>
2026-04-15 14:01:20 +0000

    remove unused windows __init__ code

--- avio/__init__.py
+++ avio/__init__.py
@@ -1,12 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
-import sys
 
-if sys.platform == "win32":
-    from avio import *
-    from avio import __doc__
-else:
-    from .avio import *
-    from .avio import __doc__
+from .avio import *
+from .avio import __doc__
 
 try:
     __version__ = version("avio")
