19fa6b3caad57ad4405bb2341bf525befc3e37ce
Stephen Rhodes <sr99622@gmail.com>
2026-04-19 17:22:55 -0400

    add command line arg to example

--- examples/player.py
+++ examples/player.py
@@ -1,7 +1,14 @@
 import avio
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("name")
+args = parser.parse_args()
+
+print(args.name)
 
 #filename = "/Users/stephen/projects/wabash/assets/short.mp4"
-filename = "/Users/stephen/Movies/news.mp4"
+filename = args.name
 player = avio.Player(filename)
 player.headless = False
 player.live_stream = False
